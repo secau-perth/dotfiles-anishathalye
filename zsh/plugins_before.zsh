d49a9f7d42ccf14bc51634a8d76ad0c6e9b361cc
p31x1 <null@p30x0.com>
2021-07-02 15:14:47 +0800

    plugins

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -1,4 +1,34 @@
-# External plugins (initialized before)
+# External plugins (initialized after)
 
-# zsh-completions
-fpath=(~/.zsh/plugins/zsh-completions/src $fpath)
+plugins=(archlinux git systemd yapipenv zsh_reload fast-syntax-highlighting history-substring-search)
+
+function is_plugin() {
+  local base_dir=$1
+  local name=$2
+  test -f $base_dir/plugins/$name/$name.plugin.zsh || test -f $base_dir/plugins/$name/_$name
+}
+
+for plugin ($plugins); do
+  if is_plugin $ZSH_CUSTOM $plugin; then
+    fpath=($ZSH_CUSTOM/plugins/$plugin $fpath)
+  elif is_plugin $ZSH $plugin; then
+    fpath=($ZSH/plugins/$plugin $fpath)
+  else
+    echo "[oh-my-zsh] plugin '$plugin' not found"
+  fi
+done
+
+
+for plugin ($plugins); do
+    if [[ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]]; then
+        source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
+    elif [[ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]]; then
+        source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+    fi
+done
+
+# dircolors
+
+if [[ "$(tput colors)" == "256" ]]; then
+    eval $(dircolors =(cat ~/.shell/plugins/dircolors-solarized/dircolors.256dark ~/.shell/dircolors.extra))
+fi
